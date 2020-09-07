2a2cb17ad53abde0109c3daad0eba475c53edfeb
Sakti <firmansyah.sakti14@gmail.com>
2020-09-07 22:46:27 +0700

    second commit

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 
-var indexRouter = require('./routes/index');
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'Sakti',
+  host: 'localhost',
+  database: 'pmsdb',
+  password: '12345',
+  port: 5432,
+})
+
+var indexRouter = require('./routes/index')(pool);
+var projectsRouter = require('./routes/projects')(pool);
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -16,19 +29,24 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser());  
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'mosafir'
+}))
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/projects', projectsRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,55 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+module.exports = (db) => {
 
-module.exports = router;
+  router.get('/', function (req, res, next) {
+    res.render('login', { loginInfo: req.flash('loginInfo') })
+  });
+
+  router.post('/login', function (req, res, next) {
+    const { email, password } = req.body
+    db.query('SELECT * FROM users WHERE email= $1', [email], (err, data) => {
+      if (err) {
+        req.flash('loginInfo', 'Ooops... Something Wrong, please call administrator!');
+        return res.redirect('/');
+      };
+
+      if (data.rows.length == 0) {
+        req.flash('logininfo', 'Wrong email or password');
+        return res.redirect('/');
+      };
+
+      bcrypt.compare(password, data.rows[0].password, function (err, isValid) {
+        console.log(isValid)
+        if (err) {
+          req.flash('loginInfo', 'Ooops... Something Wrong, please call administrator!');
+          return res.redirect('/');
+
+        };
+
+        if (!isValid) {
+          req.flash('loginInfo', 'Wrong email or password');
+          return res.redirect('/');
+        }
+
+        req.session.user = data.rows[0]
+        res.redirect('/projects');
+
+      });
+
+    });
+  });
+
+  router.get('/logout', function (req, res, next) {
+    req.session.destroy(function (err) {
+      if (err) return res.send(err)
+      res.redirect('/')
+    })
+
+  });
+
+  return router;
+}
